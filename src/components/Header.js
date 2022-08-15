374d170011004a96a27b3611f87c26454f1600ae
eimaam <imamddahir@gmail.com>
2022-08-15 13:13:22 +0100

    mobile responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-        <h1>Rick &amp; Morty</h1>
+        <h1>Rick &amp; Morty.</h1>
+        <p><mark>Rick and Morty</mark> is an American adult animated science-fiction sitcom created by Justin Roiland and Dan Harmon for Cartoon Network's nighttime programming block Adult Swim. The series follows the misadventures of cynical mad scientist Rick Sanchez and his good-hearted but fretful grandson Morty Smith, who split their time between domestic life and interdimensional adventures.</p>
     </header>
   )
 }
